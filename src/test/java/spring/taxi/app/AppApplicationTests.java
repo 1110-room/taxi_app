c4e4404efd9aee9f5d861e513322b069ad711dde
bridges123 <i.s.andreevmail.rut@gmail.com>
2022-11-25 18:37:59 +0300

    init

---
+++ src/test/java/spring/taxi/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.taxi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
