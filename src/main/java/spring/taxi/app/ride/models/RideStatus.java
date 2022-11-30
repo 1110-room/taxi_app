92726767c7c1322d69477e34c41fac0640e1fcca
bridges123 <i.s.andreevmail.rut@gmail.com>
2022-11-30 19:32:43 +0300

    finished taxi-services parser

--- src/main/java/spring/taxi/app/ride/models/RideStatus.java
+++ src/main/java/spring/taxi/app/ride/models/RideStatus.java
@@ -1,9 +1,17 @@
 package spring.taxi.app.ride.models;
 
 
+import java.util.Arrays;
+
 public enum RideStatus {
     OPEN,
     FULL,
     MOVE,
-    FINISH
+    FINISH;
+
+    public static boolean isRideStatus(String status) {
+        return Arrays.stream(RideStatus.values()).anyMatch(
+                rideStatus -> rideStatus.name().equals(status)
+        );
+    }
 }
--- src/main/java/spring/taxi/app/ride/util/TaxiParser.java
+++ src/main/java/spring/taxi/app/ride/util/TaxiParser.java
@@ -1,15 +1,36 @@
 package spring.taxi.app.ride.util;
 
 import com.google.gson.Gson;
+import spring.taxi.app.ride.util.taxi.TaxiSerivce;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class TaxiParser {
-    public static TaxiModel getInfo() throws IOException {
-        Process p = Runtime.getRuntime().exec("python3 python/parse.py");
+    public static TaxiModel getOptimalServiceInfo(double[][] coords) throws IOException, NoSuchElementException {
+        return getTotalInfoByServices(coords).stream().min(
+                Comparator.comparing(taxi -> taxi.getEconom().getPrice())
+        ).orElseThrow();
+    }
+
+    public static List<TaxiModel> getTotalInfoByServices(double[][] coords) throws IOException {
+        List<TaxiModel> taxiInfo = new ArrayList<>();
+        for (TaxiSerivce taxiSerivce : TaxiSerivce.values()) {
+            taxiInfo.add(parseInfo(taxiSerivce, coords));
+        }
+        return taxiInfo;
+    }
+
+    public static TaxiModel parseInfo(TaxiSerivce taxiService, double[][] coords) throws IOException {
+        Formatter formatter = new Formatter(Locale.US);
+        String command = formatter.format(
+                "python3 python/parse.py %s %f %f %f %f",
+                taxiService.name(), coords[0][0], coords[0][1], coords[1][0], coords[1][1]
+        ).toString();
+        Process p = Runtime.getRuntime().exec(command);
 
         InputStream stdout = p.getInputStream();
         InputStreamReader isr = new InputStreamReader(stdout);
@@ -22,9 +43,13 @@ public class TaxiParser {
             builder.append(line);
         }
 
+        System.out.println(builder);
+
         Gson gson = new Gson();
 
         TaxiModel taxiData = gson.fromJson(builder.toString(), TaxiModel.class);
+        System.out.println(taxiData.getSerivce());
+        System.out.println(taxiData.getEconom().getDistance());
         return taxiData;
     }
 }
